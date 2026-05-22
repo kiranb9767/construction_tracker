14d4ee4e3db1618852c5099501cc00aad0ca742a
Kiran Burle <Kiran.Burle@tpvaoc.com>
2026-05-22 23:35:20 +0530

    env Fix bugs

--- frontend/src/services/apiConfig.jsx
+++ frontend/src/services/apiConfig.jsx
@@ -1,9 +1,4 @@
 import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-
 
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
--- frontend/src/services/chatService.jsx
+++ frontend/src/services/chatService.jsx
@@ -1,6 +1,3 @@
-import dotenv from "dotenv";
-
-dotenv.config();
 
 let socket = null;
 
